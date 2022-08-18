6323c5d89b7825b6f88713aca02ca150415c21ab
FrontEdd <edgar@urbaner.com>
2022-08-17 23:18:28 -0500

    Conociendo condicionales: bucles for y while, if elif else

---
+++ Semana 1/Miercoles/Condicionales/condicionales.py
@@ -0,0 +1,14 @@
+##  CONDICIONALES (if, else, else if)
+##  
+
+numero = int(input('Ingrese un numero: '))
+
+#if numero == 10 :
+#    print('Cumple la condición')
+#elif numero < 10:
+#    print('Cumple la condición menor')
+#else:
+#    print('No cumple la condición')
+
+#   *condicion ? true : false
+print('Verdadero' if numero > 10 else 'Falso')

32131d513619fa55466a33843a7c8bc2eadba019
FrontEdd <edgar@urbaner.com>
2022-08-23 19:17:27 -0500

    Actualizamos renombre de carpeta y carpeta de Lunes - pendiente

---
+++ Semana 2/1-Lunes/lectura_archivos.py
@@ -0,0 +1,10 @@
+## Función 'open(file, mode)'
+#   -> Donde vamos a enviar dos <Argumentos>:
+#       1- file: ubicación y nombre del archivo | si sólo vamos a colocar el nombre del archivo (file) debe estar en la misma raiz del script de python
+#       2- mode: es el permiso con el que se abrirá dicho archivo (lectura y escritura)
+            ## r -> lectura
+            ## a ->
+            ## w ->
+            ## x ->
+
+
